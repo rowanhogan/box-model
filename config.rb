4c4fb099f1db739cf1f1f7de6b4068f8ddb1143f
Rowan Hogan <rowan.hogan@gmail.com>
2016-03-31 22:30:31 -0400

    Link fixes

--- config.rb
+++ config.rb
@@ -13,15 +13,17 @@ helpers do
       klass = (link == page_classes[/(\S+\s+){#{1}}/].strip ? 'active' : nil)
     end
 
-    link_to name.capitalize, "/#{link}", class: klass
+    link_to name.capitalize, "#{config[:host]}#{link}", class: klass
   end
 end
 
 configure :development do
   activate :livereload
+  config[:host] = "/"
 end
 
 configure :build do
   activate :minify_css
   activate :minify_javascript
+  config[:host] = "https://rowanhogan.github.io/box-model/"
 end
